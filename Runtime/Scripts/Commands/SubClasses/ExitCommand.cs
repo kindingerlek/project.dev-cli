8ce742cb59129cf7fe1bc539c494929339cde634
Lucas Ernesto Kindinger <kindingerlek@hotmail.com>
2024-01-23 18:51:12 -0300

    FEAT: create a ExitCommand

---
+++ Runtime/Scripts/Commands/SubClasses/ExitCommand.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Tools.DevConsole.Interfaces;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Tools.DevConsole.Commands
+{
+    public class ExitCommand : BaseCommand
+    {
+        public ExitCommand(IConsoleLog logger) : base(logger)
+        {
+        }
+
+        public override string Alias => "quit";
+
+        public override string Description => "Exits the application";
+
+        protected override bool Handle(ArgumentsList args, OptionsList opts)
+        {
+            logger.Log("Exiting application...");
+
+            #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+            #else
+            Application.Quit();
+            #endif
+            return true;
+        }
+    }
+}
